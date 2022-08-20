cd1128acdf3600f825c769371747cb768910a261
Tanish Mahajan <107791383+nakshtra1@users.noreply.github.com>
2022-08-20 19:32:45 +0530

    seconf commit

--- MyPersonalTodo/settings.py
+++ MyPersonalTodo/settings.py
@@ -120,18 +120,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_DIRS =(
-        os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_DIRS =(
+#         os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
